835105eadfcc75774636870d44acc372e286d337
lm2203793 <lindsaymoses@gmail.com>
2021-09-02 20:16:26 -0700

    Signed-off-by: lm2203793 <lindsaymoses@gmail.com>

---
+++ Homework/Hello_World/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: Lindsay Kislingbury
+ * Purpose: Hello World Program
+ * Created on September 2, 2021, 8:12 PM
+ */
+
+//System Libraries
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+
+    //Display Message
+    cout<<"Hello World! <(^.^)>";
+    
+    //Exit
+    return 0;
+}
+
